5124dc8db56f196cca82d0919aa5705b5dc803a1
Robin Zhou <robin.zhou@blackboard.com>
2018-01-19 11:11:46 +0800

    More errors handling.

--- CountryConverter.py
+++ CountryConverter.py
@@ -2,6 +2,7 @@
 
 from urlparse import urlparse
 import urllib2
+import socket
 import json
 
 
@@ -9,6 +10,7 @@ def convertFromUrl(httpUrl):
 	parseResult = urlparse(httpUrl)
 	IPOrHostname = parseResult.hostname
 	if IPOrHostname is None:
+		print "Error: cannot acqure IP or hostname from host url: %s" % httpUrl
 		return "ERROR_NONE_HOST"
 	else:
 		return convertFromIPOrHostname(parseResult.hostname)
@@ -17,12 +19,16 @@ def convertFromUrl(httpUrl):
 def convertFromIPOrHostname(IPOrHostname):
 	url = "http://freegeoip.net/json/" + IPOrHostname
 	try:
-		response = urllib2.urlopen(url, timeout = 5)
-		countryCode = json.loads(response.read())['country_code']
+		print url
+		response = urllib2.urlopen(url, timeout = 10)
+		responseData = response.read()
+		countryCode = json.loads(responseData)['country_code']
 		if (countryCode is None or countryCode == ''):
 			return "ERROR_FREEGEOIP_NOT_FOUND"
 		else:
 			return countryCode
+
+		print countryCode
 	except urllib2.HTTPError as e:
 		if e.code == 404:
 			print "Error: target not found from freegeoip: %s" % IPOrHostname
@@ -31,6 +37,11 @@ def convertFromIPOrHostname(IPOrHostname):
 			print "Error: exceed max numbers of attempt for FREEGEOIP API"
 			return "ERROR_FREEGEOIP_FORBBIDEN"
 		else:
-			print "Error: UNKOWN for %s \n" % IPOrHostname
-			print e
-			return "ERROR_FREEGEOIP_UNKOWN"
+			print "Error: unknown http error for %s \n" % IPOrHostname
+			return "ERROR_FREEGEOIP_UNKOWN_HTTPERROR"
+	except socket.timeout as e:
+		print "Error: timeout for hostname lookup: %s" % IPOrHostname
+		return "ERROR_FREEGEOIP_TIMEOUT"
+	except:
+		print "Error: unrecognized error for %s \n" % IPOrHostname
+		return "ERROR_FREEGEOIP_UNKOWN_ERROR"
--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # domain2CountryCode
+
+## System Requirements
+- Python 2.7
+- MySQL 5.5 or later
+- Mac or Linux
+
+## Run on local
+```bash
+python main.py
+```
